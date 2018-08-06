8e336938887b42d53e335bb155fb80bcd38f03f1
Yabo Li <liyaboleo@hotmail.com>
2018-08-06 17:24:21 +0800

    DEV: Switched to py2neo
    1. Switched from neo4j-driver to py2neo
    2. Added "mysql_data.csv"

--- app/main.py
+++ app/main.py
@@ -1,27 +1,19 @@
-from flask import Flask, request, g, jsonify
-from neo4j.v1 import GraphDatabase
-from neo4j.util import watch
+from flask import Flask, request, jsonify
+from py2neo import Graph
 import logging
-from sys import stdout
 import csv
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 uri = "bolt://192.168.99.100:7687"
-driver = GraphDatabase.driver(uri, auth=("neo4j", "passwrd"))
-watch("neo4j.bolt", logging.DEBUG, stdout)
 
 
-def get_db():
-    if not hasattr(g, 'neo4j_db'):
-        g.neo4j_db = driver.session()
-    return g.neo4j_db
-
-
-# @app.teardown_appcontext
-def close_db():
-    if hasattr(g, 'neo4j_db'):
-        g.neo4j_db.close()
+def get_graph():
+    global uri
+    uri += "/db/data/"
+    graph = Graph(uri)
+    return graph
 
 
 @app.route('/')
@@ -32,8 +24,8 @@ def hello_world():
 @app.route('/update')
 def update_db():
     # 清空数据库
-    db = get_db()
-    db.run("match (n) detach delete n")
+    graph = get_graph()
+    graph.delete_all()
 
     # 从csv文件中读取数据
     with open('resources/mysql_data_test.csv', encoding='utf-8-sig') as csvfile:
@@ -43,31 +35,31 @@ def update_db():
             labels = row['label']
             year = row['year']
             # 插入数据
-            insert_into_db(metaid, labels, year, db)
+            insert_into_db(metaid, labels, year, graph)
     return "Database updated", 200
 
 
-def insert_into_db(metaid, labels, year, db):
-    db.run("merge (b:Book {metaid: '" + metaid + "'}) "
-           "merge (y:Year {value: toInt(" + year + ")}) "
-           "merge (b)-[:PUBLISHED_IN]->(y)")
+def insert_into_db(metaid, labels, year, graph):
+    graph.run("merge (b:Book {metaid: '" + metaid + "'}) "
+              "merge (y:Year {value: toInt(" + year + ")}) "
+              "merge (b)-[:PUBLISHED_IN]->(y)")
     lst = labels.split('&')
     for label in lst:
-        db.run("merge (b:Book {metaid: '" + metaid + "'}) "
-               "merge (l:Label {name: '" + label + "'}) "
-               "merge (b)-[:HAS]->(l)")
+        graph.run("merge (b:Book {metaid: '" + metaid + "'}) "
+                  "merge (l:Label {name: '" + label + "'}) "
+                  "merge (b)-[:HAS]->(l)")
 
 
 @app.route('/query', methods=['POST'])
 def query():
     request_json = request.json
     labels = request_json['labels'].split('&')
-    db = get_db()
-    results = db.run("match (a:Book)-[:HAS]->(l:Label), "
-                     "(a)-[:PUBLISHED_IN]->(y:Year)  "
-                     "where l.name in " + str(labels) +
-                     " and y.value > 2015 "
-                     " return a.metaid")
+    graph = get_graph()
+    results = graph.run("match (a:Book)-[:HAS]->(l:Label), "
+                        "(a)-[:PUBLISHED_IN]->(y:Year)  "
+                        "where l.name in " + str(labels) +
+                        " and y.value > 2015 "
+                        " return a.metaid")
     records = {}
     for record in results:
         metaid = record["a.metaid"]
